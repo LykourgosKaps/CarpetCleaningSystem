916e5b149aaa041a0c211bee0ff77d99259af610
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-20 20:55:47 +0200

    Add customer repository abstraction for create customer use case

--- CarpetCleaningSystem.Application/Abstractions/Repositories/ICustomerRepository.cs
+++ CarpetCleaningSystem.Application/Abstractions/Repositories/ICustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarpetCleaningSystem.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace CarpetCleaningSystem.Application.Abstractions.Repositories
 {
-    internal class ICustomerRepository
+    public interface ICustomerRepository
     {
+        Task<bool> ExistsByPhoneAsync(string phoneNumber, CancellationToken ct);
+        Task AddAsync(Customer customer, CancellationToken ct);
     }
 }
