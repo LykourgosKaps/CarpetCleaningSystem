552a936e9dbecb03c8fb0a06faa35c6d2f6f0c84
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-22 15:25:16 +0200

    feat(api): configure db context, di and swagger

--- CarpetCleaningSystem.API/Program.cs
+++ CarpetCleaningSystem.API/Program.cs
@@ -1,3 +1,8 @@
+using CarpetCleaningSystem.Application.Abstractions.Repositories;
+using CarpetCleaningSystem.Infrastructure.Persistence;
+using CarpetCleaningSystem.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace CarpetCleaningSystem.API
 {
     public class Program
@@ -6,37 +11,33 @@ namespace CarpetCleaningSystem.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
+            //  DbContext
+            builder.Services.AddDbContext<AppDBContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            //  Repositories & UoW
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //  API stuff
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            });
 
             app.Run();
         }
     }
 }
+
