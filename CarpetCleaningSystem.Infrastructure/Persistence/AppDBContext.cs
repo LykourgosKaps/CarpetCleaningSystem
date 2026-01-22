e4707a1f65709433881f29e9317329f07a83612e
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-22 11:48:50 +0200

    feat(persistence): add AppDbContext and EF Core setup

---
+++ CarpetCleaningSystem.Infrastructure/Persistence/AppDBContext.cs
@@ -0,0 +1,29 @@
+﻿using CarpetCleaningSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Infrastructure.Persistence
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(AppDBContext).Assembly
+            );
+        }
+    }
+}
