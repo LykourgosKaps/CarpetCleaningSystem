e7c4a0656334ec96fd01fc829ad66739dd559e06
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-24 08:21:27 +0200

    feat(api): setup controllers and dependency injection

---
+++ CarpetCleaningSystem.API/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using CarpetCleaningSystem.Application.Customers.CreateCustomer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpetCleaningSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CreateCustomerHandler _handler;
+
+        public CustomersController(CreateCustomerHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] CreateCustomerCommand command, CancellationToken ct)
+        {
+            var response = await _handler.Handle(command, ct);
+            return Created(string.Empty, response);
+
+        }
+    }
+}
