3b675cab0e171853a1b3ecde5906bce461028e4c
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-24 10:00:07 +0200

    feat(exceptions): add CustomerNotFoundException

---
+++ CarpetCleaningSystem.Application/Exceptions/CustomerNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Exceptions
+{
+    public class CustomerNotFoundException : Exception
+    {
+        public CustomerNotFoundException(int customerId)
+            : base($"Customer with ID {customerId} was not found.")
+        {
+        }
+    }
+}
