0c0edb7876710f8bc70d82baa94c9c8070c8fa4e
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-27 15:06:39 +0200

    feat(application): add CreateOrderResponse

---
+++ CarpetCleaningSystem.Application/Orders/CreateOrder/CreateOrderResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Orders.CreateOrder
+{
+    public class CreateOrderResponse
+    {
+        public int OrderId { get; }
+    }
+}
