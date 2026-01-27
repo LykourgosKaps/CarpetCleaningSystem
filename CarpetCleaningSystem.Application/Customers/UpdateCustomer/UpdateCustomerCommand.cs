aebe8e82fa6b7468f5d34ae35cb65afe114c98f7
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-27 11:36:09 +0200

    feat(application): add UpdateCustomerCommand

---
+++ CarpetCleaningSystem.Application/Customers/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Customers.UpdateCustomer
+{
+    public class UpdateCustomerCommand
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string Address { get; set; } = string.Empty;
+
+    }
+}
