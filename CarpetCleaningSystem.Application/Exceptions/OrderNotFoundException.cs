fafa37cef3e52100962285692ca1d871e277fd13
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-27 19:05:30 +0200

    feat(application): add OrderNotFoundException

---
+++ CarpetCleaningSystem.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Exceptions
+{
+    public class OrderNotFoundException : Exception
+    {
+        public OrderNotFoundException(int orderId)
+            : base($"Order with ID {orderId} was not found.")
+        { }
+    }
+}
