7e576a71397e39b905c98c16fc4eb7730dd42050
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-27 15:24:57 +0200

    feat(api): add OrdersController with create order endpoint

---
+++ CarpetCleaningSystem.API/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using CarpetCleaningSystem.Application.Orders.CreateOrder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpetCleaningSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly CreateOrderHandler _handler;
+
+        public OrdersController(CreateOrderHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommand command, CancellationToken ct)
+        {
+            var response = await _handler.Handle(command, ct);
+            return Created(string.Empty, response);
+
+        }
+    }
+}
