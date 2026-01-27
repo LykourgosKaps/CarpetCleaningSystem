44ce00aaf6e3fe9c068e7207e51d46356f4650eb
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-27 18:21:54 +0200

    feat(application): add OrderItemResponse DTO

---
+++ CarpetCleaningSystem.Application/Orders/GetOrderById/OrderItemResponse.cs
@@ -0,0 +1,28 @@
+﻿using CarpetCleaningSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Orders.GetOrderById
+{
+    public class OrderItemResponse
+    {
+        public int CarpetLabelNumber { get; }
+
+        public CleaningType CleaningType { get; }
+
+        public decimal Price { get; }
+
+        public OrderItemResponse(
+            int carpetLabelNumber,
+            CleaningType cleaningType,
+            decimal price)
+        {
+            CarpetLabelNumber = carpetLabelNumber;
+            CleaningType = cleaningType;
+            Price = price;
+        }
+    }
+}
