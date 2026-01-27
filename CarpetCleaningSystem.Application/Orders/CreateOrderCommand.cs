4b3708e1bb150068bbcc9786d7d1614915a8b623
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-27 14:29:09 +0200

    feat(application): add CreateOrderCommand with optional pickup date

---
+++ CarpetCleaningSystem.Application/Orders/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Orders
+{
+    public class CreateOrderCommand
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int CustomerId { get; set; }
+
+        public DateTime? PickUpDate { get; set; }
+    }
+}
