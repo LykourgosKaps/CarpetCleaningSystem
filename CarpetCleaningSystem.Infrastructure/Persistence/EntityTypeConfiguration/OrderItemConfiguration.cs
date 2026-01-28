72d9477cae6672225de441f11828a26406aeda9e
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-28 12:38:41 +0200

    feat(persistence): add OrderItem configuration with primary key and relationship

---
+++ CarpetCleaningSystem.Infrastructure/Persistence/EntityTypeConfiguration/OrderItemConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using CarpetCleaningSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Infrastructure.Persistence.EntityTypeConfiguration
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            // PK (global unique)
+            builder.HasKey(x => x.CarpetLabelNumber);
+
+            builder.Property(x => x.CarpetLabelNumber)
+                   .IsRequired();
+
+            builder.Property(x => x.CleaningType)
+                   .IsRequired()
+                   .HasConversion<int>();
+
+            builder.Property(x => x.Price)
+                   .IsRequired()
+                   .HasPrecision(18, 2);
+
+            // Shadow FK προς Order
+            builder.Property<int>("OrderId");
+
+            builder.HasIndex("OrderId");
+
+            builder.HasOne<Order>()
+                   .WithMany("Items")
+                   .HasForeignKey("OrderId")
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
