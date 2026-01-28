22ecdfaf37cbd830be31f23d197e1802fdf24f90
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-28 15:45:18 +0200

    fix(persistence): correct DbSet name for Carpet

--- CarpetCleaningSystem.Infrastructure/Persistence/AppDBContext.cs
+++ CarpetCleaningSystem.Infrastructure/Persistence/AppDBContext.cs
@@ -19,7 +19,7 @@ namespace CarpetCleaningSystem.Infrastructure.Persistence
 
         public DbSet<Order> Orders => Set<Order>();
 
-        public DbSet<Carpet> Services => Set<Carpet>();
+        public DbSet<Carpet> Carpets => Set<Carpet>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
