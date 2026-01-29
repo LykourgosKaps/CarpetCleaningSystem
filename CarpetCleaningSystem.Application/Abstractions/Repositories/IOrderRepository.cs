8d6644dc1c62e628ee4c6fe022f53c44ebb5ce9d
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-29 17:58:21 +0200

    fix(abstractions): remove GetByIdWithItems useless

--- CarpetCleaningSystem.Application/Abstractions/Repositories/IOrderRepository.cs
+++ CarpetCleaningSystem.Application/Abstractions/Repositories/IOrderRepository.cs
@@ -12,7 +12,5 @@ namespace CarpetCleaningSystem.Application.Abstractions.Repositories
         Task AddAsync(Order order, CancellationToken cancellationToken);
 
         Task<Order?> GetByIdAsync(int orderId, CancellationToken cancellationToken);
-
-        Task<Order?> GetByIdWithItemsAsync(int orderId, CancellationToken cancellationToken);
     }
 }
