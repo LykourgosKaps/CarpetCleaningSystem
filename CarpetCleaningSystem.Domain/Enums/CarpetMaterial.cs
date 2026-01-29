090cdd3bfd37b5d5895e4ec368b1a6992745de4e
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-29 17:29:17 +0200

    refactor(domain): rename CarpetMaterial enum to ItemType

--- CarpetCleaningSystem.Domain/Enums/CarpetMaterial.cs
+++ CarpetCleaningSystem.Domain/Enums/CarpetMaterial.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarpetCleaningSystem.Domain.Enums
 {
-    public enum CarpetMaterial
+    public enum ItemType
     {
         SYNTHETIC,
         HANDMADE_WOOL,
