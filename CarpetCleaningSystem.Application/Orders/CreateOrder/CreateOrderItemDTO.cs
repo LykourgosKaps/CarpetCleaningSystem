f0154a38a5e98420106696c0889cea438409af62
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-30 15:42:36 +0200

    chore(application): add validation to CreateOrderItemDTO

--- CarpetCleaningSystem.Application/Orders/CreateOrder/CreateOrderItemDTO.cs
+++ CarpetCleaningSystem.Application/Orders/CreateOrder/CreateOrderItemDTO.cs
@@ -1,6 +1,7 @@
 ﻿using CarpetCleaningSystem.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,19 @@ namespace CarpetCleaningSystem.Application.Orders.CreateOrder
 {
     public class CreateOrderItemDTO
     {
+        [Range(0.01, 100.00)]
         public decimal Width { get; set; }
 
+        [Range(0.01, 100.00)]
         public decimal Length { get; set; }
 
+        [Required]
+        [EnumDataType(typeof(ItemType))]
         public ItemType Material { get; set; }
 
+        [Required]
+        [EnumDataType(typeof(CleaningType))]
         public CleaningType CleaningType { get; set; }
     }
+
 }
