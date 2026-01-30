f56f7dae17408aa07be43b76eb4be477c3f5126f
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-30 10:02:20 +0200

    feat(application): add OrderItemsLockedException

---
+++ CarpetCleaningSystem.Application/Exceptions/OrderItemsLockedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Exceptions
+{
+    public class OrderItemsLockedException : InvalidOperationException
+    {
+        public OrderItemsLockedException()
+            : base("Order items cannot be modified after processing has started.")
+        {
+        }
+    }
+
+}
