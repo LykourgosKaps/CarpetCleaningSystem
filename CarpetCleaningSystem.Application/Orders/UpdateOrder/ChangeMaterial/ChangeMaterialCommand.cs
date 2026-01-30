c72f706bacf751256539b0e695693bf8d47741ee
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-30 11:49:33 +0200

    fix(application): return 404 when order item is missing on ChangeMaterial

--- CarpetCleaningSystem.Application/Orders/UpdateOrder/ChangeMaterial/ChangeMaterialCommand.cs
+++ CarpetCleaningSystem.Application/Orders/UpdateOrder/ChangeMaterial/ChangeMaterialCommand.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using CarpetCleaningSystem.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CarpetCleaningSystem.Application.Orders.UpdateOrder.ChangeMaterial
 {
-    internal class ChangeMaterialCommand
+    public class ChangeMaterialCommand
     {
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int OrderId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int OrderItemId { get; set; }
+
+        [Required]
+        public ItemType newMaterial { get; set; }
     }
 }
--- CarpetCleaningSystem.Application/Orders/UpdateOrder/ChangeMaterial/ChangeMaterialHandler.cs
+++ CarpetCleaningSystem.Application/Orders/UpdateOrder/ChangeMaterial/ChangeMaterialHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarpetCleaningSystem.Application.Abstractions.Repositories;
+using CarpetCleaningSystem.Application.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace CarpetCleaningSystem.Application.Orders.UpdateOrder.ChangeMaterial
 {
-    internal class ChangeMaterialHandler
+    public class ChangeMaterialHandler
     {
+        private readonly IOrderRepository _orderRepository;
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ChangeMaterialHandler(
+            IOrderRepository orderRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _orderRepository = orderRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(ChangeMaterialCommand request, CancellationToken ct)
+        {
+            var order = await _orderRepository.GetByIdAsync(request.OrderId, ct)
+                ?? throw new OrderNotFoundException(request.OrderId);
+
+            try
+            {
+                order.ChangeItemMaterial(request.OrderItemId, request.newMaterial);
+                await _unitOfWork.SaveChangesAsync(ct);
+            }
+            catch (ArgumentException ex) when (ex.ParamName == "orderItemId")
+            {
+                throw new OrderItemNotFoundException(request.OrderItemId);
+            }
+        }
     }
 }
