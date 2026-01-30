a63f8a98ca39306e1c51688513dd3e4538d6c742
Lykourgos <lykourgos.kaps@outlook.com>
2026-01-30 21:14:35 +0200

    feat(application): add pricing service abstractions

---
+++ CarpetCleaningSystem.Application/Abstractions/Pricing/IPricingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpetCleaningSystem.Application.Abstractions.Services
+{
+    public interface IPricingService
+    {
+        decimal CalculateItemPrice(ItemPricingInput input);
+
+        decimal CalculateOrderTotal(IEnumerable<ItemPricingInput> items);
+    }
+
+}
---
+++ CarpetCleaningSystem.Application/Abstractions/Pricing/ItemPricingInput.cs
@@ -0,0 +1,11 @@
+﻿using CarpetCleaningSystem.Domain.Enums;
+
+namespace CarpetCleaningSystem.Application.Pricing
+{
+    public record ItemPricingInput(
+        decimal Surface,
+        ItemType ItemType,
+        CleaningType CleaningType
+    );
+}
+
